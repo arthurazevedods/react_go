5a3a4cdc837ab2ec868c9ad359469443e5d23471
arthur <arthurazevedods@gmail.com>
2025-04-19 12:18:00 -0300

    setup & .get

--- main.go
+++ main.go
@@ -11,12 +11,15 @@ func main() {
 
 	app.Get("/", func(c fiber.Ctx) error {
 		// Send a string response to the client
-		return c.SendString("Hello, World 👋!")
+		return c.SendString("Hello,GOLANG WORLD!!")
+	})
+	app.Get("/hello", func(c fiber.Ctx) error {
+		return c.Status(200).JSON(fiber.Map{"msg": "hello postman"})
 	})
 
 	app.Get("/teste", func(c fiber.Ctx) error {
 		// Send a string response to the client
-		return c.SendString("Teste")
+		return c.SendString("TESTE")
 	})
 
 	// Start the server on port 3000
